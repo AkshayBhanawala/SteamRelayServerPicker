ab5e0da7e4c79876f488f979073a829985ebf598
Akshay Bhanawala <AkshayBhanawala@gmail.com>
2026-07-15 23:24:59 +0530

    Vue Import updates

--- src/Vue/components/GlobeMap.vue
+++ src/Vue/components/GlobeMap.vue
@@ -39,7 +39,7 @@
 <script setup lang="ts">
 import { ref, onMounted, onBeforeUnmount, toRaw } from 'vue';
 import * as d3 from 'd3';
-import { isMaxPing } from '../util/Common.util.ts';
+import { isMaxPing } from '../util/Common.util';
 import type { ProcessedLocation } from '../../types';
 
 const props = defineProps<{
